147883c7205234cc6ba9f14e67d220c48d84b9cd
Jeremiah Weaver <jbw71699@gmail.com>
2019-06-18 12:18:42 -0500

    Removed unused tabs. Updated shared header component to actually contain the header. Updated Home page to use shared header component.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'tc-header',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+
+  @Input()
+  public pageName: string;
 
   constructor() { }
 
