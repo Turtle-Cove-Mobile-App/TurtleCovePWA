ba7202b59c58a8c08238e45bb636028da1eb13fa
Jeremiah Weaver <jbw71699@gmail.com>
2019-09-23 16:02:29 -0500

    Updated filterByStartsWithPipe to return null if the filter resulted in an empty array.

--- src/app/shared/pipes/filter-by-starts-with.pipe.ts
+++ src/app/shared/pipes/filter-by-starts-with.pipe.ts
@@ -7,13 +7,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterByStartsWithPipe implements PipeTransform {
 
   transform(array, letter): any {
-    return array.filter(eachItem => {
+    const filterArray = array.filter(eachItem => {
       return (
         eachItem.toLowerCase().startsWith(
           letter.toLowerCase()
         )
       );
     });
+    if (filterArray.length > 0) {
+      return filterArray;
+    }
+    else {
+      return null;
+    }
   }
 
 }
