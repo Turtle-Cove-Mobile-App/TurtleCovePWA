ccf8aa16f267b52bd1c27cb04701dcf6381fe01f
Jeremiah Weaver <jbw71699@gmail.com>
2019-09-23 17:37:54 -0500

    Updated species classes to be expandable and updated list formatting.

--- src/app/pages/checklist/checklist.page.ts
+++ src/app/pages/checklist/checklist.page.ts
@@ -14,22 +14,27 @@ export class ChecklistPage implements OnInit {
   public species = [
     {
       class: 'Fish/Macroinvertebrate',
-      species: ['One Fish', 'Two Fish', 'Red Fish', 'Blue Fish']
+      expanded: false,
+      species: ['One Fish', 'O2 Fish', 'O3 Fish', 'Two Fish', 'Red Fish', 'Blue Fish']
     },
     {
       class: 'Reptiles',
+      expanded: false,
       species: ['Snek', 'Lizard Person']
     },
     {
       class: 'Plants',
+      expanded: false,
       species: ['Grass', 'Happy Little Tree', 'Flower']
     },
     {
       class: 'Birds',
+      expanded: false,
       species: ['Screeching Owl', 'Bald Eagle']
     },
     {
       class: 'Mammals',
+      expanded: false,
       species: ['Pao', 'Red Head Canadian', 'McDowell', 'G-Doc', 'Zac']
     }
   ];
@@ -37,4 +42,8 @@ export class ChecklistPage implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+toggleClass(animalClass) {
+    animalClass.expanded = !animalClass.expanded;
+  }
 }
