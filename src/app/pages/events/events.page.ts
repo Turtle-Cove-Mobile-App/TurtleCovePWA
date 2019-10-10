2f24b7ac05f4f46aff52906f0bfbc60b0e30c22e
Jeremiah Weaver <jbw71699@gmail.com>
2019-10-09 22:12:44 -0500

    Added some mapping for google calendar api response. Began adding some basic frontend ui for the events page.

--- src/app/pages/events/events.page.ts
+++ src/app/pages/events/events.page.ts
@@ -1,28 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'tc-events',
-  templateUrl: './events.page.html',
-  styleUrls: ['./events.page.scss'],
+  selector: "tc-events",
+  templateUrl: "./events.page.html",
+  styleUrls: ["./events.page.scss"]
 })
 export class EventsPage implements OnInit {
+  private apiKey = "AIzaSyDxJFgb7IlKXSGritDAsMtmbWZ7P4rLfqU";
 
-  private apiKey = 'AIzaSyDxJFgb7IlKXSGritDAsMtmbWZ7P4rLfqU';
+  public events;
 
-  private events;
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.getEvents();
   }
 
   async getEvents() {
-
-    await fetch('https://www.googleapis.com/calendar/v3/calendars/ok6j0uq2hpfl5u2883mn0poeh8%40group.calendar.google.com/events?key=' + this.apiKey)
-    .then(res => res.json().then(resJson => this.events = resJson));
+    await fetch(
+      "https://www.googleapis.com/calendar/v3/calendars/ok6j0uq2hpfl5u2883mn0poeh8%40group.calendar.google.com/events?key=" +
+        this.apiKey
+    ).then(res =>
+      res.json().then(resJson => {
+        this.events = resJson.items.map(item => {
+          return {
+            date: item.start.date,
+            summary: item.summary,
+            description: item.description || null,
+            htmlLink: item.htmlLink
+          };
+        });
+      })
+    );
 
     console.log(this.events);
   }
-
 }
