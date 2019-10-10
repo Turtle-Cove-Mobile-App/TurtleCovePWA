ed73050ded1ec75261e9df1c5bed1ee055a846d2
Jeremiah Weaver <jbw71699@gmail.com>
2019-10-09 21:49:46 -0500

    Implemented google calendar events api.

--- src/app/pages/events/events.page.ts
+++ src/app/pages/events/events.page.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsPage implements OnInit {
 
+  private apiKey = 'AIzaSyDxJFgb7IlKXSGritDAsMtmbWZ7P4rLfqU';
+
+  private events;
+
   constructor() { }
 
   ngOnInit() {
+    this.getEvents();
+  }
+
+  async getEvents() {
+
+    await fetch('https://www.googleapis.com/calendar/v3/calendars/ok6j0uq2hpfl5u2883mn0poeh8%40group.calendar.google.com/events?key=' + this.apiKey)
+    .then(res => res.json().then(resJson => this.events = resJson));
+
+    console.log(this.events);
   }
 
 }
