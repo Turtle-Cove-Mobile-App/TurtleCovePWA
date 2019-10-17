744d9b15f187fe24ab6f5aa45f80dcffa13e9ceb
Zac Armand <31135590+ZacNotZach@users.noreply.github.com>
2019-10-17 15:35:57 -0500

    properly formatted tc-tour page (#19)

--- src/app/pages/tc-tour/tc-tour.page.ts
+++ src/app/pages/tc-tour/tc-tour.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'tc-tc-tour',
@@ -7,9 +8,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TcTourPage implements OnInit {
 
-  constructor() { }
+  public total = 12; // setTotal?
+  public found = [];
+  public totalFound = 0;
+
+  public primColor = 'primary';
+
+  public signs = [
+    {
+      id: 0,
+      name: '1',
+      viewed: false
+    },
+    {
+      id: 1,
+      name: '2',
+      viewed: false
+    },
+    {
+      id: 2,
+      name: '3',
+      viewed: false
+    },
+    {
+      id: 3,
+      name: '4',
+      viewed: false
+    },
+    {
+      id: 4,
+      name: '5',
+      viewed: false
+    },
+    {
+      id: 5,
+      name: '6',
+      viewed: false
+    },
+    {
+      id: 6,
+      name: '7',
+      viewed: false
+    },
+    {
+      id: 7,
+      name: '8',
+      viewed: false
+    },
+    {
+      id: 8,
+      name: '9',
+      viewed: false
+    },
+    {
+      id: 9,
+      name: '10',
+      viewed: false
+    },
+    {
+      id: 10,
+      name: '11',
+      viewed: false
+    },
+    {
+      id: 11,
+      name: '12',
+      viewed: false
+    }
+  ];
+
+  constructor/*(public popoverController: PopoverController, private storage: Storage, private alertController: AlertController)*/() {}
+
+  public getColor(viewed): string {
+    if (viewed) {
+      return 'medium';
+    } else {
+      return 'primary';
+    }
+  }
+  public signViewed(obj): void {
+    if (obj.viewed === false) {
+      obj.viewed = true;
+      this.totalFound++;
+    }
+  }
+
+  public setTotal(): void {
+    for (const o of this.signs) {
+      this.total++;
+    }
+  }
+
+  public reset(): void {
+
+    for (const o of this.signs) {
+        o.viewed = false;
+    }
+    this.primColor = 'primary';
+    this.totalFound = 0;
+  }
 
   ngOnInit() {
+    // this.setTotal;
   }
 
 }
