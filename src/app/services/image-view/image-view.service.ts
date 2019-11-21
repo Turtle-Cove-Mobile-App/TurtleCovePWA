0a28c5648863f5a0d4d9e22432e469934e4a2a25
Jeremiah Weaver <jbw716@jeremiahs-mbp.lan>
2019-11-20 20:36:45 -0600

    Updated image view service to be more generic.

--- src/app/services/image-view/image-view.service.ts
+++ src/app/services/image-view/image-view.service.ts
@@ -5,42 +5,42 @@ import { Injectable } from '@angular/core';
 })
 export class ImageViewService {
 
-  public signs: any[];
-  public total: number = 15;
+  public images: any[];
+  public total: number;
   public totalViewed: number;
 
   constructor() { }
 
-  public setSigns(signs: any[]) {
-    this.signs = signs;
+  public setImages(images: any[]) {
+    this.images = images;
     this.calcTotal();
     this.calcTotalViewed();
   }
 
   public calcTotal() {
-    this.total = this.signs.length;
+    this.total = this.images.length;
     return this.total;
   }
 
   public calcTotalViewed() {
     this.totalViewed = 0;
-    for (const sign of this.signs) {
-      if (sign.viewed === true) {
+    for (const image of this.images) {
+      if (image.viewed === true) {
         this.totalViewed++;
       }
     }
   }
 
-  public viewSign(id): void {
-    if (this.signs[id].viewed === false) {
-      this.signs[id].viewed = true;
+  public viewImage(id) {
+    if (this.images[id].viewed === false) {
+      this.images[id].viewed = true;
       this.totalViewed++;
     }
   }
 
   reset() {
-    for (const sign of this.signs) {
-      sign.viewed = false;
+    for (const image of this.images) {
+      image.viewed = false;
     }
     this.calcTotalViewed();
   }
