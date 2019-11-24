c092f938b4f47e30ff4d9c8fe4bd4c43b9873a83
Jeremiah Weaver <jbw716@jeremiahs-mbp.lan>
2019-11-23 21:22:13 -0600

    Prettied up slider.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,6 @@ export class AppComponent implements OnInit {
   }
 
   initializeApp() {
-    console.log(this.platform.platforms());
     StatusBar.setStyle({
       style: StatusBarStyle.Dark
     });
