8d1f46a2e9486289e001c42619036206224abce0
Zac Armand <31135590+ZacNotZach@users.noreply.github.com>
2019-12-05 00:14:19 -0600

    Added Toast Popups when No Internet Connection (#39)

--- src/app/pages/info/info.page.ts
+++ src/app/pages/info/info.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'tc-info',
@@ -9,8 +11,40 @@ export class InfoPage implements OnInit {
 
   public historyExpanded = false;
   public whatGoesOnExpanded = false;
+  private isToastOpen = false;
+  public online = window.navigator.onLine;
 
-  constructor() { }
+  constructor(public toastController: ToastController) { }
+
+  async presentToastWithOptions() {
+    console.log(this.isToastOpen);
+    if (!this.isToastOpen) {
+      const toast = await this.toastController.create({
+        header: 'Failed to load...',
+        message: 'This feature requires an internet connection',
+        color: 'dark',
+        position: 'bottom',
+        buttons: [
+          {
+            text: 'Close',
+            handler: () => {
+              console.log('Cancel clicked');
+              this.isToastOpen = false;
+            }
+          }
+        ]
+      });
+      toast.present().then(() => this.isToastOpen = true);
+    }
+  }
+
+  buttonHandler() {
+    if (this.online) {
+      window.open('https://goo.gl/maps/mqcqMtYzFSaTokYRA', '_blank');
+    } else {
+      this.presentToastWithOptions();
+    }
+  }
 
   toggleHistory() {
     if (this.historyExpanded === false) {
